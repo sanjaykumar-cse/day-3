2cd61e1ed1e14906b39a1981fc6eee7079c973bd
sanjaykumar-cse <111355256+sanjaykumar-cse@users.noreply.github.com>
2022-09-21 17:00:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var arr =[1,2,3,4,5];
+for (var i in arr) {
+    console.log(i);
+}
+
+
+for(var i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+
+
+var arr =[1,2,3,4,5];
+for (var z of arr) {
+    console.log(z);
+}
